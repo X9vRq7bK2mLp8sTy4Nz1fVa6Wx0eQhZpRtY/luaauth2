f6cf093bd05be15586cf1e2ef7f45f63647fa79a
AMIXNSUJNJNSIXNSIHXNSUHBXEUHBUHNKSJN <jawshcx@icloud.com>
2025-10-29 13:53:00 +0200

    Delete api/utils.js

--- api/utils.js
+++
@@ -1,9 +0,0 @@
-const allowedUAs = ['synapse', 'krnl', 'fluxus', 'script-ware', 'scriptware', 'sentinel', 'executor', 'roblox', 'exploit', 'electron'];
-
-function isAllowedUA(userAgent) {
-  if (!userAgent) return false;
-  userAgent = userAgent.toLowerCase();
-  return allowedUAs.some(sub => userAgent.includes(sub));
-}
-
-module.exports = { isAllowedUA };
